b9e28c4066731435dafbe7558efef76aeddd8f78
vcolonnella <folial@gmail.com>
2018-10-27 03:10:08 +0200

    Final working version

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import drawSvg as draw
 nsq = 4
 mw = 15
 mh = 32
-msg = [["ALZA LA FIAMMA AGLI DEI", "MOSTRA ALLA NOTTE CHI SEI", "TU NON SAPEVI TU SAI", "PADRE DEL FUOCO SARAI"],
-       ["HO DORMITO NEL SOLE", "HO GUIDATO DI NOTTE", "HO ASPETTATO IL MATTINO", "DAL LATO SBAGLIATO"],
-       ["DISSE IL SOLDATO AL SUO RE", "NUOVO MONDO TU AVRAI", "DAMMI TEMPO", "E VEDRAI"],
-       ["BATTE IL SUO TEMPO SEMPRE ESATTO", "IL FORTE TAMBURO DEL PETTO", "CUORE DI GUERRA SEMPRE ATTENTO", "CUORE DI QUERCIA NEL VENTO"]]
+msg = [["PRIMA IL GELATO POI IL DENTISTA", "FATTI I CAPELLI ROSSI",
+    "RIPARA LE TUE SCARPE", "AGGIUSTA MOTO E BICI"],
+    ["IMPARA A GUIDARE", "LAVI E IGIENIZZI DA SOLO",
+    "IL BAR DEL PARCO VICINO", "ARREDA LA TUA CASA"],
+    ["VINO SFUSO FRESCO", "UN MARE DI SOLDI",
+    "SPEDISCI PACCHI E LETTERE", "ODORE DI GIADA"],
+    ["LIBRI E PENNE PER COPIARE", "IMPARA INGLESE CON AMICI",
+    "TI ASSISTE IN PANNE", "STAMPA SU OGNI OGGETTO"]]
 canvas = [[['_' for c1 in range(mw)] for c2 in range(mh)] for c3 in range(nsq)]
 
 def printcanvas(c):
@@ -16,7 +20,7 @@ def printcanvas(c):
         for y in x:
             out += y
         out+="\n"
-    print(out)
+    return out
 
 def outtransf(arr, pos, mode):
     oy = int(pos / mw)
@@ -45,6 +49,10 @@ def outtransf_idx(pos, mode):
         oy = mh - oy - 1
     return oy * mw + ox
 
+fc = open("canvas.txt", "w")
+fs = open("solution.txt", "w")
+ff = open("nonsense.txt", "w")
+
 # disponi msg su canvas tracciando le posizioni
 # la distanza di ogni passo varia attorno alla distanza media su tutta la matrice (15 * 32 / length(msg))
 # senza superare la media dello spazio restante
@@ -66,27 +74,24 @@ for sq in range(nsq):
             sol[sq][j][x] = cvj
             canvas[sq][int(cvj / mw)][cvj % mw] = msg[sq][j][x]
 
-#printcanvas(canvas)
-#print(pos)
-
 # riempi lo spazio restante di caratteri casuali
     for y in range(mh):
         for x in range(mw):
             if canvas[sq][y][x] == '_':
                 canvas[sq][y][x] = chr(65 + random.randint(0, 25))
-    print("--- Map {}".format(sq))
-    printcanvas(canvas[sq])
+    fc.write("--- Map {}\n".format(sq))
+    fc.write("{}\n".format(printcanvas(canvas[sq])))
 
 # costruisci maschere spurie distinte da posizioni
     fal[sq] = []
-    for x in range(8):
+    for x in range(2):
         falr = []
         for y in range(random.randint(16, 24)):
             falr.append(random.randint(0, mw * mh - 1))
         fal[sq].append(falr)
 
 # verifica tutte le disposizioni delle maschere solutive producendo il relativo output
-print("--- Solution")
+fs.write("--- Solution")
 for sq in range(nsq):
     for k, o in enumerate(sol[sq]):
         v1 = v2 = v3 = v4 = ''
@@ -97,10 +102,10 @@ for sq in range(nsq):
                 v2 += outtransf(canvas[sq], i, 'w')
                 v3 += outtransf(canvas[sq], i, 'h')
                 v4 += outtransf(canvas[sq], i, 'wh')
-        print(v1 + "\n" + v2 + "\n" + v3 + "\n" + v4)
+        fs.write("{}\n{}\n{}\n{}\n".format(v1, v2, v3, v4))
 
 # verifica tutte le disposizioni spurie di maschere producendo il relativo output
-print("--- Check nonsense")
+ff.write("--- Check false")
 for sk in range(nsq):
     for sq in range(nsq):
         for k, o in enumerate(fal[sk]):
@@ -112,7 +117,11 @@ for sk in range(nsq):
                     v2 += outtransf(canvas[sq], i, 'w')
                     v3 += outtransf(canvas[sq], i, 'h')
                     v4 += outtransf(canvas[sq], i, 'wh')
-            print(v1 + "\n" + v2 + "\n" + v3 + "\n" + v4 + "\n")
+            ff.write("{}\n{}\n{}\n{}\n".format(v1, v2, v3, v4))
+
+fc.close()
+fs.close()
+ff.close()
 
 # draw canvas
 bo=[[9, 9], [102, 9], [195, 9]]
@@ -137,21 +146,43 @@ if ro != 2:
     d.clear()
 
 # draw solution masks
+mode = ['', 'w', 'h', 'wh']
 for c3 in range(nsq):
     for c2 in range(len(sol[c3])):
         roc = len(sol[0]) * c3 + c2
         ro = roc % 3
-        r = draw.Rectangle(bo[ro][0], bo[ro][1], 90, 192, fill='transparent', stroke='#333333', stroke_width=0.2)
+        r = draw.Rectangle(bo[ro][0], bo[ro][1], 90, 192, fill='transparent', stroke='#FF0000', stroke_width=0.2)
         d.append(r)
+        th = draw.Text("S-{}.{}".format(c3, c2), 3, bo[ro][0], bo[ro][1] - 5, fill='black', stroke='transparent')
+        d.append(th)
         for c1 in range(len(sol[c3][c2])):
             if sol[c3][c2][c1] != -1:
-                cc = outtransf_xy(sol[c3][c2][c1], '')
-                c = draw.Circle(bo[ro][0] + 3 + 6 * cc[0], bo[ro][1] + 3 + 6 * (mh - cc[1] - 1), 3, fill='transparent', stroke='#333333', stroke_width=0.2)
+                cc = outtransf_xy(sol[c3][c2][c1], mode[(c2 + c3) % 4])
+                c = draw.Circle(bo[ro][0] + 3 + 6 * cc[0], bo[ro][1] + 3 + 6 * (mh - cc[1] - 1), 3, fill='transparent', stroke='#FF0000', stroke_width=0.2)
                 d.append(c)
         if ro == 2:
             d.savePng('solution{}_{}.png'.format(c3, c2))
             d.clear()
 if ro != 2:
     d.savePng('solution{}_{}.png'.format(c3, c2))
+    d.clear()
+
+# draw nonsense masks
+for c3 in range(nsq):
+    for c2 in range(len(fal[c3])):
+        roc = len(fal[0]) * c3 + c2
+        ro = roc % 3
+        r = draw.Rectangle(bo[ro][0], bo[ro][1], 90, 192, fill='transparent', stroke='#FF0000', stroke_width=0.2)
+        d.append(r)
+        for c1 in range(len(fal[c3][c2])):
+            if fal[c3][c2][c1] != -1:
+                cc = outtransf_xy(fal[c3][c2][c1], '')
+                c = draw.Circle(bo[ro][0] + 3 + 6 * cc[0], bo[ro][1] + 3 + 6 * (mh - cc[1] - 1), 3, fill='transparent', stroke='#FF0000', stroke_width=0.2)
+                d.append(c)
+        if ro == 2:
+            d.savePng('nonsense{}_{}.png'.format(c3, c2))
+            d.clear()
+if ro != 2:
+    d.savePng('nonsense{}_{}.png'.format(c3, c2))
     d.clear()
 
